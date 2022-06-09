f193b69ffac7a348a3630b914279cf5a53ca8d7b
Mariana Ferreira <mariana.ballardin@hotmail.com>
2022-06-09 15:28:13 +0200

    alteração no alerta
    adicionei if else e removi a função

---
+++ desafio.md
@@ -0,0 +1,13 @@
+Bora praticar e rever tudo o que foi ensinado na aula? **💜**
+
+Nesse desafio você irá criar uma lista de **estudantes** e, cada estudante dentro dessa lista, deverá conter os seguintes dados:
+
+- nome;
+- nota da primeira prova;
+- nota da segunda prova.
+
+Depois de criada a lista:
+
+- [ ]  Crie uma **função** que irá calcular a média das notas de cada aluno;
+- [ ]  Supondo que a média, para esse concurso é **7**, verifique **se** cada aluno obteve sucesso ou não em entrar no concurso e mostre uma mensagem na tela.
+
---
+++ script.js
@@ -0,0 +1,64 @@
+const students = [
+  
+{ 
+  nome: "Sebastian",
+  firstGrade: 10,
+  secondGrade: 10,
+},
+
+{
+  nome: "Mariana",
+  firstGrade: 10,
+  secondGrade: 10,
+},
+
+{  nome: "Mariela",
+  firstGrade: 9,
+  secondGrade: 1,
+},
+
+{
+  nome: "Marília",
+  firstGrade: 7,
+  secondGrade: 6,
+},
+
+{ 
+  nome: "Tuga",
+  firstGrade: 3,
+  secondGrade: 2,
+},
+]
+
+
+// alert(`
+// A sua média, ${students[0].nome}, é ${(students[0].fistGrade + students[0].secondGrade) / 2}.`)
+
+
+
+function mediaFinal(firstGrade, secondGrade) {
+  return Number(((firstGrade + secondGrade) / 2))
+}
+
+if mediaFinal > 7 {
+  alert( 
+    `Caro(a) aluno(a) ${student.nome}, sua média final é ${mediaFinal(student.firstGrade, student.secondGrade)}. Parabéns, voce foi aprovado!`)
+}
+
+else {
+  alert( 
+    `Caro(a) aluno(a) ${student.nome}, sua média final é ${mediaFinal(student.firstGrade, student.secondGrade)}. Infelizmente você não foi aprovado!`)
+
+}
+
+// function printMedia(student) {
+//   return `
+//   Caro(a) aluno(a) ${student.nome}, sua média final é ${mediaFinal(student.firstGrade, student.secondGrade)}`
+// }
+
+
+for (let student of students) {
+  let mediaFinalMessage = printMedia(student)
+  alert(mediaFinalMessage)
+}
+
